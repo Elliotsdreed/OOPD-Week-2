f2c48a6e23f7e00bdd7195953eb4259dbc1b3af4
Elliotsdreed <elliotsdreed@icloud.com>
2019-10-04 14:00:12 +0100

    parent and child class

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace OOPD_Week_2
+{
+    public class ParentClass
+    {
+        public ParentClass()
+        {
+            Console.WriteLine("Creating an instance of Parent");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("I am a Parent");
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+    public class ChildClass : ParentClass
+    {
+        public ChildClass()
+        {
+            Console.WriteLine("Creating an instance of Child");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("I am a Child");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParentClass parent = new ParentClass();
+            ChildClass child = new ChildClass();
+
+            child.SayHello();
+        }
+    }
+}
